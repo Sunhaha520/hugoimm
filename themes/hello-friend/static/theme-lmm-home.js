199e3dbc55e11ec8a3091ac5c1c1655994c8ea92
Sunhaha520 <67938343+Sunhaha520@users.noreply.github.com>
2024-01-17 16:30:28 +0800

    Update theme-lmm-home.js

--- themes/hello-friend/static/theme-lmm-home.js
+++ themes/hello-friend/static/theme-lmm-home.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
 var bbDom = document.querySelector('#bber-talk') || '';
 if(bbDom){memoTalks();}
 function memoTalks(){
-var bbUrl = "https://bibi.xiaoayu.ren/api/v1/memo?creatorId=1&rowStatus=NORMAL&limit=10"
+var bbUrl = "https://lonely-susann-cola.koyeb.app/api/v1/memo?creatorId=1&rowStatus=NORMAL&limit=10"
 fetch(bbUrl).then(res => res.json()).then( resdata =>{
     var result = '',resultAll="",data = resdata
     for(var i=0;i < data.length;i++){
@@ -30,7 +30,7 @@ setInterval(function() {
 }
 //memos 相册
 var albumDom = document.querySelector('#album') || '';
-var memoUrl = "https://bibi.xiaoayu.ren/"
+var memoUrl = "https://lonely-susann-cola.koyeb.app/"
 if(albumDom){memoAlbum(7);}
 function memoAlbum(numb){
     let limit = numb || 8;
